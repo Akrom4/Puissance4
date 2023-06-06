53ae6b5deb5e539179e7bcf5276d1990fc1ec125
Fabrice Chaplain <75309565+Akrom4@users.noreply.github.com>
2023-06-06 13:39:04 +0200

    WIP : alpha beta

--- models/Engine.js
+++ models/Engine.js
@@ -1,139 +1,152 @@
 class Engine {
-    game;
-    level;
-    maxDepth;
+  game;
+  level;
+  maxDepth;
 
-    constructor(game, level) {
-        this.game = game;
-        this.level = level;
-        this.maxDepth = this.level * 2;
-    }
+  constructor(game, level) {
+    this.game = game;
+    this.level = level;
+    this.maxDepth = 2;
+  }
 
-    /* 
+  /* 
     The Engine selects a move based on his level
     */
-    makeDecision() {
-        // Level 0 random play
-        if (this.level === 1) {
-            let validColumns = [];
-            for (let i = 0; i < this.game.getColumns(); i++) {
-                if (this.game.getEmptySlotIndex(this.game.getPosition()[i]) !== -1) {
-                    validColumns.push(i);
-                }
-            }
-            let randomColumnIndex = Math.floor(Math.random() * validColumns.length);
-            return validColumns[randomColumnIndex];
+  makeDecision() {
+    // Level 0 random play
+    if (this.level === 1) {
+      let validColumns = [];
+      for (let i = 0; i < this.game.getColumns(); i++) {
+        if (this.game.getEmptySlotIndex(this.game.getPosition()[i]) !== -1) {
+          validColumns.push(i);
         }
-        // For levels greater than 0, use Minimax algorithm
-        else {
-            console.log("minimax")
-            let bestScore = -Infinity;
-            let move;
+      }
+      let randomColumnIndex = Math.floor(Math.random() * validColumns.length);
+      return validColumns[randomColumnIndex];
+    }
+    // For levels greater than 0, use Minimax algorithm
+    else {
+      let bestScore = -Infinity;
+      let move;
 
-            for (let i = 0; i < this.game.getColumns(); i++) {
-                // Make a move
-                this.game.playMove(i);
-                let score = this.minimax(this.maxDepth, -Infinity, Infinity, false);
-                // Undo the move
-                this.game.undoMove();
-                if (score > bestScore) {
-                    bestScore = score;
-                    move = i;
-                }
-            }
-            console.log(move);
-            this.game.toLog();
-            return move;
+      for (let i = 0; i < this.game.getColumns(); i++) {
+        // Check if column is full
+        if (this.game.getEmptySlotIndex(this.game.getPosition()[i]) === -1) {
+          continue;
         }
+        // Make a move
+        this.game.playMove(i);
+        let score = this.minimax(this.maxDepth, -Infinity, Infinity, true);
+        // Undo the move
+        this.game.undoMove();
+        if (score > bestScore) {
+          bestScore = score;
+          move = i;
+        }
+      }
+      return move;
+    }
+  }
+
+  /* MiniMax algorithm */
+  minimax(depth, alpha, beta, maximizingPlayer) {
+    // Get the current player and opponent before simulating a move
+    let currentPlayer = this.game.getTurn() === "Y" ? "R" : "Y";
+
+    // Clone the game
+    // let gameClone = this.game.clone();
+
+    // If at terminal node or depth = 0, return heuristic value
+    if (depth === 0 || this.game.checkWinCondition() !== null) {
+      return this.evaluate(this.game, currentPlayer);
     }
 
-
-    /* MiniMax algorithm */
-    minimax(depth, alpha, beta, maximizingPlayer) {
-        // Get the current player and opponent before simulating a move
-        let currentPlayer = this.game.getTurn();
-        let opponentPlayer = this.game.getTurn() === "Y" ? "R" : "Y";
-    
-        // Clone the game
-        let gameClone = this.game.clone();
-        // If at terminal node or depth = 0, return heuristic value
-        if (depth === 0 || gameClone.checkWinCondition() !== null) {
-            return this.evaluate(gameClone, currentPlayer, opponentPlayer);
-        }
-
-    let columnOrder = this.middleFirstColumnOrder(gameClone.getColumns());
-
+    let columnOrder = this.middleFirstColumnOrder(this.game.getColumns());
     if (maximizingPlayer) {
-        let maxEval = -Infinity;
-        for (let i of columnOrder) {
-            // Check if the column is not full
-            if (gameClone.getEmptySlotIndex(gameClone.getPosition()[i]) !== -1) {
-                // Make a move on the cloned game
-                gameClone.playMove(i);
-                let evaluation = this.minimax(depth - 1, alpha, beta, false);
-                // Undo the move on the cloned game
-                gameClone.undoMove();
-                maxEval = Math.max(maxEval, evaluation);
-                alpha = Math.max(alpha, evaluation);
-                // Alpha Beta Pruning
-                if (beta <= alpha) {
-                    break;
-                }
-            }
+      let maxEval = -Infinity;
+      for (let i of columnOrder) {
+        // Check if the column is not full
+        if (this.game.getEmptySlotIndex(this.game.getPosition()[i]) !== -1) {
+          // Make a move on the cloned game
+          this.game.playMove(i);
+          let evaluation = this.minimax(depth - 1, alpha, beta, false);
+          // Undo the move on the cloned game
+          this.game.undoMove();
+          maxEval = Math.max(maxEval, evaluation);
+          alpha = Math.max(alpha, evaluation);
+          // Alpha Beta Pruning
+          if (beta <= alpha) {
+            break;
+          }
         }
-        return maxEval;
+      }
+      return maxEval;
     } else {
-        let minEval = Infinity;
-        for (let i of columnOrder) {
-            // Check if the column is not full
-            if (gameClone.getEmptySlotIndex(gameClone.getPosition()[i]) !== -1) {
-                // Make a move on the cloned game
-                gameClone.playMove(i);
-                let evaluation = this.minimax(depth - 1, alpha, beta, true);
-                // Undo the move on the cloned game
-                gameClone.undoMove();
-                minEval = Math.min(minEval, evaluation);
-                beta = Math.min(beta, evaluation);
-                // Alpha Beta Pruning
-                if (beta <= alpha) {
-                    break;
-                }
-            }
+      let minEval = Infinity;
+      for (let i of columnOrder) {
+        // Check if the column is not full
+        if (this.game.getEmptySlotIndex(this.game.getPosition()[i]) !== -1) {
+          // Make a move on the cloned game
+          this.game.playMove(i);
+          let evaluation = this.minimax(depth - 1, alpha, beta, true);
+          // Undo the move on the cloned game
+          this.game.undoMove();
+          minEval = Math.min(minEval, evaluation);
+          beta = Math.min(beta, evaluation);
+          // Alpha Beta Pruning
+          if (beta <= alpha) {
+            break;
+          }
         }
-        return minEval;
+      }
+      return minEval;
     }
-}
+  }
 
-
-    /* Column Order helper function */
-    middleFirstColumnOrder(columns) {
-        let order = [];
-        let middle = Math.floor(columns / 2);
-        for (let offset = 0; offset <= middle; offset++) {
-            if (middle - offset >= 0) {
-                order.push(middle - offset);
-            }
-            if (middle + offset < columns) {
-                order.push(middle + offset);
-            }
-        }
-        return order;
+  /* Column Order helper function */
+  middleFirstColumnOrder(columns) {
+    let order = [];
+    let middle = Math.floor(columns / 2);
+    for (let offset = 0; offset <= middle; offset++) {
+      if (middle - offset >= 0) {
+        order.push(middle - offset);
+      }
+      if (middle + offset < columns && offset !== 0) {
+        order.push(middle + offset);
+      }
     }
+    return order;
+  }
 
-
-    /* Heuristic evaluation function */
-    evaluate(game, currentPlayer, opponentPlayer) {
-        // If the AI wins, return 1
-        if (game.checkWinCondition(currentPlayer)) {
-            return 1;
-        }
-        // If the game is lost, return -1
-        else if (game.checkWinCondition(opponentPlayer)) {
-            return -1;
-        }
-        // If the game is not yet finished, return 0
-        else {
-            return 0;
-        }
+  /* Heuristic evaluation function */
+evaluate(game, currentPlayer) {
+    console.log("Evaluation :");
+    
+    let opponentPlayer = currentPlayer === "Y" ? "R" : "Y";
+    
+    // If the AI wins, return 1
+    if (game.checkWinCondition(currentPlayer) !== null) {
+    //   console.log("Currentplayer", currentPlayer);
+    //   console.log("Position", game.position);
+      if (currentPlayer == "R") {
+        return 1;
+      } else {
+        return 0;
+      }
     }
+    // If the opponent wins, return -1
+    else if (game.checkWinCondition(opponentPlayer) !== null) {
+    //   console.log("OpponentPlayer", opponentPlayer);
+    //   console.log("Position", game.position);
+      if (opponentPlayer == "R") {
+        return -1;
+      } else {
+        return 0;
+      }
+    }
+    // If the game is not yet finished, return 0
+    else {
+      return 0;
+    }
+  }  
 }
--- models/Game.js
+++ models/Game.js
@@ -1,199 +1,205 @@
 /* Game logic */
 
 class Game {
-    rows;
-    columns;
-    position;
-    moveHistory; // Saves the last column played. From left to right starting with the index 1.
-    turn; // Y for Yellow player || R for Red player (used to know whose turn it is and token color placement)
-    winCondition;
+  rows;
+  columns;
+  position;
+  moveHistory; // Saves the last column played. From left to right starting with the index 1.
+  turn; // Y for Yellow player || R for Red player (used to know whose turn it is and token color placement)
+  winCondition;
 
-    constructor(rows, columns) {
-        this.rows = rows;
-        this.columns = columns;
-        this.winCondition = this.setWinCondition(this.columns, this.rows);
-        this.position = [];
-        this.moveHistory = [];
+  constructor(rows, columns) {
+    this.rows = rows;
+    this.columns = columns;
+    this.winCondition = this.setWinCondition(this.columns, this.rows);
+    this.position = [];
+    this.moveHistory = [];
 
-        for (let i = 0; i < this.columns; i++) {
-            const column = [];
-            for (let j = 0; j < this.rows; j++) {
-                column.push(" ");
-            }
-            this.position.push(column);
-        }
-        this.turn = "Y";
+    for (let i = 0; i < this.columns; i++) {
+      const column = [];
+      for (let j = 0; j < this.rows; j++) {
+        column.push(" ");
+      }
+      this.position.push(column);
     }
+    this.turn = "Y";
+  }
 
-    playMove(colNum) {
-        // Get the selected column
-        const column = this.position[colNum];
+  playMove(colNum) {
+    // Get the selected column
+    const column = this.position[colNum];
 
-        // Find the first empty slot in the column
-        const emptySlotIndex = this.getEmptySlotIndex(column);
-        if (emptySlotIndex !== -1) {
-            // Place the token of the current player
-            column[emptySlotIndex] = this.turn;
-            const lastTurn = this.turn; // Save the color of the last turn
-            this.moveHistory.push(colNum + 1);
-            // Switch the turn to the other player
-            this.switchTurn();
-            return lastTurn;
-        }
-        else {
-            return false;
-        }
+    // Find the first empty slot in the column
+    const emptySlotIndex = this.getEmptySlotIndex(column);
+    if (emptySlotIndex !== -1) {
+      // Place the token of the current player
+      column[emptySlotIndex] = this.turn;
+      const lastTurn = this.turn; // Save the color of the last turn
+      this.moveHistory.push(colNum + 1);
+      // Switch the turn to the other player
+      this.switchTurn();
+      return lastTurn;
+    } else {
+      return false;
     }
+  }
 
-    /* Win condition */
+  /* Win condition */
 
-    checkWinCondition(lastTurn) {
-        if (this.moveHistory.length === 0) {
-            return null; // no moves have been made yet
-        }
-        const lastMoveColumn = this.moveHistory[this.moveHistory.length - 1] - 1; // Get the column of the last move
-        if (lastMoveColumn < 0 || lastMoveColumn >= this.columns) {
-            return null; // invalid column index
-        }
-        const lastMoveRow = this.position[lastMoveColumn].lastIndexOf(this.turn === "Y" ? "R" : "Y"); // Get the row index of the last move
-
-        // Check for a win condition
-        const directions = [
-            [-1, 0], // horizontal
-            [0, -1], // vertical
-            [-1, -1], // diagonal ↘
-            [-1, 1], // diagonal ↗
-        ];
-
-        for (let [dx, dy] of directions) {
-            let count = 0;
-            let winningSequence = [];
-            for (
-                let offset = -this.winCondition - 1;
-                offset <= this.winCondition + 1;
-                offset++
-            ) {
-                let nx = lastMoveColumn + offset * dx;
-                let ny = lastMoveRow + offset * dy;
-                if (
-                    nx >= 0 &&
-                    nx < this.columns &&
-                    ny >= 0 &&
-                    ny < this.rows &&
-                    this.position[nx][ny] === lastTurn
-                ) {
-                    count++;
-                    winningSequence.push([nx, ny]);
-                    if (count === this.winCondition) {
-                        return winningSequence; // return the winning sequence
-                    }
-                } else {
-                    count = 0;
-                    winningSequence = []; // reset the sequence when the chain is broken
-                }
-            }
-        }
-        return null; // no winner
+  checkWinCondition(lastTurn) {
+    if (this.moveHistory.length === 0) {
+      return null; // no moves have been made yet
     }
-
-    /* Set the number of aligned tokens to win the game */
-    setWinCondition(x, y) {
-        let win = Math.floor(Math.sqrt(x * y) - 2);
-        return win;
+    const lastMoveColumn = this.moveHistory[this.moveHistory.length - 1] - 1; // Get the column of the last move
+    if (lastMoveColumn < 0 || lastMoveColumn >= this.columns) {
+      return null; // invalid column index
     }
-    undoMove() {
-        // Only attempt to undo if there has been at least one move
-        if (this.moveHistory.length > 0) {
-            // Get the column of the last move
-            const lastMoveColumn = this.moveHistory[this.moveHistory.length - 1] - 1;
+    const lastMoveRow = this.position[lastMoveColumn].lastIndexOf(
+      this.turn === "Y" ? "R" : "Y"
+    ); // Get the row index of the last move
 
-            // Find the first non-empty slot in the column (from bottom to top)
-            const column = this.position[lastMoveColumn];
-            const filledSlotIndex = column.lastIndexOf(this.turn === "Y" ? "R" : "Y");
+    // Check for a win condition
+    const directions = [
+      [-1, 0], // horizontal
+      [0, -1], // vertical
+      [-1, -1], // diagonal ↘
+      [-1, 1], // diagonal ↗
+    ];
 
-            // If found, empty the slot
-            if (filledSlotIndex !== -1) {
-                column[filledSlotIndex] = " ";
-            }
-
-            // Remove the last move from the history
-            this.moveHistory.pop();
-
-            // Switch the turn to the other player
-            this.switchTurn();
-
+    for (let [dx, dy] of directions) {
+      let count = 0;
+      let winningSequence = [];
+      for (
+        let offset = -this.winCondition - 1;
+        offset <= this.winCondition + 1;
+        offset++
+      ) {
+        let nx = lastMoveColumn + offset * dx;
+        let ny = lastMoveRow + offset * dy;
+        if (
+          nx >= 0 &&
+          nx < this.columns &&
+          ny >= 0 &&
+          ny < this.rows &&
+          this.position[nx][ny] === lastTurn
+        ) {
+          count++;
+          winningSequence.push([nx, ny]);
+          if (count === this.winCondition) {
+            return winningSequence; // return the winning sequence
+          }
+        } else {
+          count = 0;
+          winningSequence = []; // reset the sequence when the chain is broken
         }
+      }
     }
-    /* Switch turn */
-    switchTurn() {
-        this.turn = this.turn === "Y" ? "R" : "Y";
-    }
+    return null; // no winner
+  }
 
-    /* Returns the index of the first empty row 
+  /* Set the number of aligned tokens to win the game */
+  setWinCondition(x, y) {
+    let win = Math.floor(Math.sqrt(x * y) - 2);
+    return win;
+  }
+  undoMove() {
+    // Only attempt to undo if there has been at least one move
+    if (this.moveHistory.length > 0) {
+      // Get the column of the last move
+      const lastMoveColumn = this.moveHistory[this.moveHistory.length - 1] - 1;
+
+      // Find the first non-empty slot in the column (from bottom to top)
+      const column = this.position[lastMoveColumn];
+      const filledSlotIndex = column.lastIndexOf(this.turn === "Y" ? "R" : "Y");
+
+      // If found, empty the slot
+      if (filledSlotIndex !== -1) {
+        column[filledSlotIndex] = " ";
+      }
+
+      // Remove the last move from the history
+      this.moveHistory.pop();
+
+      // Switch the turn to the other player
+      this.switchTurn();
+    }
+  }
+  /* Switch turn */
+  switchTurn() {
+    this.turn = this.turn === "Y" ? "R" : "Y";
+  }
+
+  /* Returns the index of the first empty row 
     Returns -1 if the column is full */
-    getEmptySlotIndex(column) {
-        return column.indexOf(" ");
-    }
+  getEmptySlotIndex(column) {
+    return column.indexOf(" ");
+  }
+// /* Deep clone */
+//   clone() {
+//     let clone = _.cloneDeep(this);
+//     return clone;
+// }
 
-    /* Create a deep clone */
-    clone() {
-        let clone = new Game(this.rows, this.columns);
-        clone.position = JSON.parse(JSON.stringify(this.position));
-        clone.moveHistory = [...this.moveHistory];
-        clone.turn = this.turn;
-        clone.winCondition = this.winCondition;
-        return clone;
-    }
-    
 
-    /* GETTERS */
-    getWinCondition() {
-        return this.winCondition;
-    }
 
-    getPosition() {
-        return this.position;
-    }
+  /* GETTERS */
+  getWinCondition() {
+    return this.winCondition;
+  }
 
-    getRows() {
-        return this.rows;
-    }
+  getPosition() {
+    return this.position;
+  }
 
-    getColumns() {
-        return this.columns;
-    }
+  getRows() {
+    return this.rows;
+  }
 
-    getTurn() {
-        return this.turn;
-    }
+  getColumns() {
+    return this.columns;
+  }
 
-    /* DEBUG TOOLS */
-    /* Display the board in console */
-    toLog() {
-        console.log("Current Board Position log :");
-        for (let j = this.rows - 1; j >= 0; j--) {
-            let row = "";
-            for (let i = 0; i < this.columns; i++) {
-                row += this.position[i][j] || " ";
-                row += "\t";
-            }
-            console.log(row);
+  getTurn() {
+    return this.turn;
+  }
+
+  //   getValidColumns() {
+  //     let validColumns = [];
+  //     for (let i = 0; i < this.game.getColumns(); i++) {
+  //       if (this.game.getEmptySlotIndex(this.game.getPosition()[i]) !== -1) {
+  //         validColumns.push(i);
+  //       }
+  //     }
+  //     return validColumns;
+  //   }
+
+  /* DEBUG TOOLS */
+  /* Display the board in console */
+  toLog() {
+    console.log("Current Board Position log :");
+    for (let j = this.rows - 1; j >= 0; j--) {
+      let row = "";
+      for (let i = 0; i < this.columns; i++) {
+        row += this.position[i][j] || " ";
+        row += "\t";
+      }
+      console.log(row);
+    }
+  }
+
+  /* Create a random position */
+  testPosition() {
+    for (let i = 0; i < this.columns; i++) {
+      for (let j = 0; j < this.rows; j++) {
+        const rand = Math.random();
+        if (rand < 0.33) {
+          this.position[i][j] = " ";
+        } else if (rand < 0.66) {
+          this.position[i][j] = "Y";
+        } else {
+          this.position[i][j] = "R";
         }
+      }
     }
-
-    /* Create a random position */
-    testPosition() {
-        for (let i = 0; i < this.columns; i++) {
-            for (let j = 0; j < this.rows; j++) {
-                const rand = Math.random();
-                if (rand < 0.33) {
-                    this.position[i][j] = " ";
-                } else if (rand < 0.66) {
-                    this.position[i][j] = "Y";
-                } else {
-                    this.position[i][j] = "R";
-                }
-            }
-        }
-    }
+  }
 }
